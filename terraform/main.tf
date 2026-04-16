2af050a313609dc42f8edc566f851ed5923a89be
sampreeth B S <sampreethsampu15@gmail.com>
2026-04-16 18:51:58 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,4 @@
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "devops-aks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "devops"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-rg"
+  location = "East US"
 }
