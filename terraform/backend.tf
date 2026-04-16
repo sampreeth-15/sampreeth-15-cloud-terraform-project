c60ead6b3228aa7612d1ca2323086dce7a979315
sampreeth B S <sampreethsampu15@gmail.com>
2026-04-16 18:39:14 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-state-rg"
+    storage_account_name = "tfstatestorage123"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
